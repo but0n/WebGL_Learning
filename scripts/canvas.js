717051db8b63d21e2764595f7c755dcd4a6f2903
but0n <but0n@icloud.com>
2017-11-09 23:31:03 +0800

    feat: (Triangle) Color

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -34,12 +34,12 @@ let attrloc = gl.getAttribLocation(shaderProgram, "pos");
 // Create buffer
 let buf = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buf);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0,0, -0.5,0.8, 0.5,0, 0,0.8]), gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-0.3,0, 0.3,0, 0,0.8]), gl.STATIC_DRAW);
 gl.vertexAttribPointer(attrloc, 2, gl.FLOAT, false, 0, 0);
 
 gl.enableVertexAttribArray(attrloc);
 
-gl.drawArrays(gl.LINE_LOOP, 0, 4);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
 
 
 // for(let a = 0; a<100; a++) {
