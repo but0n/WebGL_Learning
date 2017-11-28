29d0dc1da65fc814f4c9f8eccd4dbb000ead1645
anym <anym@anym.com>
2017-11-28 20:09:26 +0800

    Spot light

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -169,6 +169,10 @@ nm.setInverseOf(model);
 nm.transpose();
 gl.uniformMatrix4fv(nmLoc, false, nm.elements);
 
+// Spot light
+let lposLoc = gl.getUniformLocation(shaderProgram, 'u_lightPosition');
+gl.uniform3f(lposLoc, 0.0, 3.0, 4.0);
+
 let ambLoc = gl.getUniformLocation(shaderProgram, "u_ambientLight");
 gl.uniform3f(ambLoc, 0.2, 0.2, 0.2);
 
