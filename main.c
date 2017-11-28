ab528febc678837e3061136a62bb616bf9deb549
anym <anym@anym.com>
2017-11-28 10:20:33 +0800

    5 queens done

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define N 8
-static unsigned short   buf[N];
+#define N 15
+static unsigned int   buf[N];
 
 #define CLEAN_BUFF() do{\
     for(unsigned char t = 0; t < N; t++)\
@@ -10,55 +10,73 @@ static unsigned short   buf[N];
 #define ABS(x) (((x)>=0)?(x):(x)*-1)
 
 
-static unsigned char isLeagul(unsigned char x, unsigned char y);
-static void q(char y);
-static void console();
+unsigned char isLeagul(unsigned int x, unsigned int y);
+static void q();
+static void console(unsigned int t);
 
 
 int main() {
     CLEAN_BUFF();
-    q(0);
-    console();
+    q();
 }
 
 
-unsigned char isLeagul(unsigned char x, unsigned char y) {
-    unsigned short coluOr = 0;
+unsigned char isLeagul(unsigned int x, unsigned int y) {
+    unsigned int coluOr = 0;
     for(unsigned char i = 0; i < N; i++) {
-        coluOr |= buf[i] & (1<<x);
+        coluOr |= buf[i] & x;
     }
-    unsigned short delta = 0;
+    unsigned int delta = 0;
     for(unsigned char i = 0; i < N; i++) {
-        delta |= ((buf[i]>>ABS(y-i)) | (buf[i]<<ABS(y-i))) & (1<<x);
+        delta |= ((buf[i]>>ABS(y-i)) | (buf[i]<<ABS(y-i))) & x;
     }
     if((buf[y]==0) && (coluOr==0) && (delta == 0))
         return 1;
     else
         return 0;
 }
-unsigned char flag = 0;
-void q(char y) {
-    if(y >= N) {
-        console();
-    }
-    for(unsigned char x = 0; x < N; x++) {
-        if(isLeagul(x, y)) {
-            buf[y] |= 1<<x;
-            q(y+1);
-            break;
+
+void q() {
+    unsigned int t = 0;
+    unsigned int y = 0, x = 1;
+    while(y < N) {
+        while(x < 1<<N) {
+            if(isLeagul(x, y)) {
+                buf[y] = x;
+                x = 1;
+                break;
+            } else {
+                x = x<<1;
+            }
         }
-        if(x==N-1) {
-            printf("Error at %d\n", y);
-            flag++;
-            y -= 1;
-            if(y<0) y=0;
-            buf[y] = buf[y]<<1;
-            q(y);
+        if(buf[y]==0) {
+            if(y == 0)
+                break;
+            else {
+                y--;
+                x = buf[y]<<1;
+                if(x==0)
+                    x=1;
+                buf[y] = 0;
+                continue;
+            }
         }
+        if(y == N-1) {
+            t++;
+            console(t);
+            x = buf[y]<<1;
+            buf[y] = 0;
+            if(t > 1000) // Fix time limmit
+                break;
+            continue;
+        }
+
+        y++;
     }
+
 }
-void console() {
-    printf("----\n");
+void console(unsigned int t) {
+    printf("%03d-----\n", t);
     for(unsigned char i = 0; i < N; i++) {
         for(unsigned char b = 0; b < N; b++)
             printf("%d ", buf[i]>>b & 1);
