4d98e60a5ce2d6f7c452237ae448bd4d0e3b84f8
but0n <but0n@icloud.com>
2017-12-02 20:37:30 +0800

    Still doesn't work :(

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -29,7 +29,7 @@ gl.useProgram(shaderProgram);
 
 // let coloc = gl.getAttribLocation(shaderProgram, "a_Color");
 
-let mod = GenerateModel(1.8, 0.8, 100);
+let mod = GenerateModel(1.8, 0.8, 200);
 let vertices = mod.vertices;
 let normals = mod.normals;
 let colors = mod.color;
@@ -115,7 +115,27 @@ gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_COLOR_BIT);
 
 gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, vmapData, gl.STATIC_DRAW);
 
+// _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
+// Create Framebuffer NOTE
+let fb = gl.createFramebuffer();
 
+// Create NOTE Texturebuffer NOTE to store color data
+let textureBuffer = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, textureBuffer);
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1024, 1024, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LIEARN);
+fb.texture = textureBuffer;
+
+// _=Create NOTE Renderbuffer NOTE to store depth data
+let depthBuffer = gl.createRenderbuffer();
+gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, 1024, 1024);
+
+// NOTE: Attach those
+gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, textureBuffer, 0);
+gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
+// _=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
 
 // Textrue
 let texture = gl.createTexture(); // Create Textrue
@@ -125,15 +145,28 @@ let textureLoc = gl.getUniformLocation(shaderProgram, 'u_sampler');
 let image = new Image();
 image.src = './hello.png';
 image.onload = () => {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_COLOR_BIT);
+
+    // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-    gl.activeTexture(gl.TEXTURE0);
+    gl.activeTexture(gl.TEXTURE1);
     gl.bindTexture(gl.TEXTURE_2D, texture);
     // Configure
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     // Sending data
-    gl.uniform1i(textureLoc, 0);
+    gl.uniform1i(textureLoc, 1);
     gl.drawElements(gl.TRIANGLES, mod.map.length, gl.UNSIGNED_SHORT, 0); // Render
+    // move();
+    // NOTE: Toggle buffer
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, fb.texture);
+    gl.uniform1i(textureLoc, 0);
+
+    // gl.drawElements(gl.TRIANGLES, mod.map.length, gl.UNSIGNED_SHORT, 0); // Render
+
 
 };
 
@@ -141,20 +174,20 @@ let last_time = Date.now();
 let move = () => {
     let d = Date.now() - last_time;
     last_time = Date.now();
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_COLOR_BIT);
+    // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_COLOR_BIT);
     model.rotate(-60/1000*d, 0, 1, 0);
     model.rotate(-60/1000*d, 1, 0, 0);
     // matrix.translate(0, 0, -1/1000*d);
     gl.uniformMatrix4fv(modelLocation, false, model.elements);
-    gl.uniformMatrix4fv(viewLocation, false, view.elements);
-    gl.uniformMatrix4fv(projeLocation, false, proje.elements);
+    // gl.uniformMatrix4fv(viewLocation, false, view.elements);
+    // gl.uniformMatrix4fv(projeLocation, false, proje.elements);
     nm.setInverseOf(model);
     nm.transpose();
     gl.uniformMatrix4fv(nmLoc, false, nm.elements);
 
     gl.drawElements(gl.TRIANGLES, mod.map.length, gl.UNSIGNED_SHORT, 0); // Render
 
-    console.log('FPS:'+Math.floor(1000/d));
+    // console.log('FPS:'+Math.floor(1000/d));
 
 
     requestAnimationFrame(move);
